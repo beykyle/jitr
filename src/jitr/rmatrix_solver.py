9eab1440e1d302f49154745babb816a60a041196
beykyle <beykyle@umich.edu>
2024-08-27 12:01:17 -0400

    fix hstack call

--- src/jitr/rmatrix_solver.py
+++ src/jitr/rmatrix_solver.py
@@ -40,11 +40,10 @@ class RMatrixSolver:
         nbasis = self.kernel.quadrature.nbasis
         nchannels = np.size(a)
         return np.hstack(
-            [
-                [self.kernel.f(n, a[i], a[i]) for n in range(1, nbasis + 1)]
+            (
+                np.array([self.kernel.f(n, a[i], a[i]) for n in range(1, nbasis + 1)], dtype=np.complex128)
                 for i in range(nchannels)
-            ],
-            dtype=np.complex128,
+            )
         )
 
     def get_channel_block(self, matrix: np.array, i: np.int32, j: np.int32 = None):
