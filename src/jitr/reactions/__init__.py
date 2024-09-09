f21a05e82de46fa3f9bf34a6928229f7701ce9eb
beykyle <beykyle@umich.edu>
2024-09-09 12:40:30 -0400

    add wavefunction to imports

--- src/jitr/reactions/__init__.py
+++ src/jitr/reactions/__init__.py
@@ -1,3 +1,4 @@
 from .channel_on_grid import make_channel_data, SingleChannelData
 from .system import ProjectileTargetSystem, Asymptotics, Channels
+from .wavefunction import Wavefunction
 from .potentials import *
