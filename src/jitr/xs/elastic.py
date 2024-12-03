90cf1c8d8b369e33d9376062e7f164479825a526
beykyle <beykyle@umich.edu>
2024-12-03 15:54:13 -0500

    separate integral and elastic observables

--- src/jitr/xs/elastic.py
+++ src/jitr/xs/elastic.py
@@ -24,22 +24,11 @@ class ElasticXS:
     rutherford: np.ndarray = None
 
 
-class Workspace:
+class IntegralWorkspace:
     r"""
-    Workspace for elastic scattering observables for local interactions with spin-orbit coupling
+    Workspace for integral observables like S-matrix elements and total and reaction cross sections for
+    local interactions with spin-orbit coupling
     """
-
-    @classmethod
-    def load(obj, filename):
-        with open(filename, "rb") as f:
-            ws = pickle.load(f)
-            ws.solver = Solver(ws.nbasis)
-        return ws
-
-    def save(self, filename):
-        self.solver = None
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
 
     def __init__(
         self,
@@ -48,19 +37,21 @@ class Workspace:
         sys: ProjectileTargetSystem,
         kinematics: ChannelKinematics,
         solver: Solver,
-        angles: np.array,
         smatrix_abs_tol: np.float64 = 1e-6,
     ):
+        # system info
         self.projectile = projectile
         self.target = target
         self.sys = sys
         self.solver = solver
         self.nbasis = solver.kernel.quadrature.nbasis
+        self.smatrix_abs_tol = smatrix_abs_tol
+
+        # kinematic info
         self.mu = kinematics.mu
         self.Ecm = kinematics.Ecm
         self.k = kinematics.k
         self.eta = kinematics.eta
-        self.smatrix_abs_tol = smatrix_abs_tol
 
         # precompute things
         self.free_matrices = self.solver.free_matrix(
@@ -78,36 +69,17 @@ class Workspace:
         self.asymptotics = [asym.decouple() for asym in asymptotics]
         self.l_dot_s = np.array([np.diag(coupling) for coupling in sys.couplings[1:]])
 
-        # preocmpute angular distributions in each partial wave
-        self.angles = angles
-        ls = self.sys.l[:, np.newaxis]
-        self.P_l_costheta = eval_legendre(ls, np.cos(self.angles))
-        self.P_1_l_costheta = lpmv(1, ls, np.cos(self.angles))
-
         # precompute things related to Coulomb interaction
+        self.ls = self.sys.l[:, np.newaxis]
         self.Zz = self.projectile[1] * self.target[1]
         if self.Zz > 0:
             self.k_c = constants.ALPHA * self.Zz * self.mu / constants.HBARC
             self.eta = self.k_c / self.k
-            self.sigma_l = np.angle(gamma(1 + ls + 1j * self.eta))
-            sin2 = np.sin(self.angles / 2.0) ** 2
-            self.f_c = (
-                -self.eta
-                / (2 * self.k * sin2)
-                * np.exp(
-                    2j * self.sigma_l[0]
-                    - 2j * self.eta * np.log(np.sin(self.angles / 2))
-                )
-            )
-            self.rutherford = (
-                10 * self.eta**2 / (4 * self.k**2 * np.sin(self.angles / 2) ** 4)
-            )
+            self.sigma_l = np.angle(gamma(1 + self.ls + 1j * self.eta))
         else:
+            self.sigma_l = np.angle(gamma(1 + self.ls + 1j * 0))
             self.k_c = 0
             self.eta = 0
-            self.sigma_l = np.angle(gamma(1 + ls + 1j * 0))
-            self.f_c = np.zeros_like(angles)
-            self.rutherford = np.zeros_like(angles)
 
     def smatrix(
         self,
@@ -116,6 +88,10 @@ class Workspace:
         args_scalar=None,
         args_spin_orbit=None,
     ):
+        r"""
+        returns the partial wave S-matrix elements as two arrays over partial
+        wave l, one for for the l+1/2 and a ssecond for the l-1/2 partial waves
+        """
         splus = np.zeros(self.sys.lmax + 1, dtype=np.complex128)
         sminus = np.zeros(self.sys.lmax + 1, dtype=np.complex128)
 
@@ -174,7 +150,123 @@ class Workspace:
             ) < self.smatrix_abs_tol:
                 break
 
-        return splus[:l], sminus[:l]
+        return splus, sminus
+
+    def xs(
+        self,
+        interaction_scalar,
+        interaction_spin_orbit,
+        args_scalar=None,
+        args_spin_orbit=None,
+        angles=None,
+    ):
+        r"""
+        returns the angle-integrated total, elastic and reaction cross sections
+        """
+        splus, sminus = self.smatrix(
+            kinematics,
+            interaction_scalar,
+            interaction_spin_orbit,
+            args_scalar,
+            args_spin_orbit,
+        )
+        return integral_elastic_xs(kinematics.k, splus, sminus, self.ls, self.sigma_l)
+
+    def transmission_coefficients(
+        self,
+        interaction_scalar,
+        interaction_spin_orbit,
+        args_scalar=None,
+        args_spin_orbit=None,
+        angles=None,
+    ):
+        r"""
+        returns the partial wave tranmission coefficients as two arrays over
+        partial wave l, one for for the l+1/2 and a second for the l-1/2
+        partial waves
+        """
+        splus, sminus = self.smatrix(
+            kinematics,
+            interaction_scalar,
+            interaction_spin_orbit,
+            args_scalar,
+            args_spin_orbit,
+        )
+        return 1.0 - np.absolute(splus) ** 2, 1.0 - np.absolute(sminus) ** 2
+
+
+class DifferentialWorkspace:
+    r"""
+    Workspace for differential elastic scattering observables for local
+    interactions with spin-orbit coupling
+    """
+
+    @classmethod
+    def build_from_system(
+        cls,
+        projectile: tuple,
+        target: tuple,
+        sys: ProjectileTargetSystem,
+        kinematics: ChannelKinematics,
+        solver: Solver,
+        angles: np.array,
+        smatrix_abs_tol: np.float64 = 1e-6,
+    ):
+        integral_workspace = IntegralWorkspace(
+            projectile,
+            target,
+            sys,
+            kinematics,
+            solver,
+            smatrix_abs_tol,
+        )
+        return cls(integral_workspace, angles, smatrix_abs_tol)
+
+    def __init__(
+        self,
+        integral_workspace: IntegralWorkspace,
+        angles: np.array,
+        smatrix_abs_tol: np.float64 = 1e-6,
+    ):
+
+        # system info
+        self.integral_workspace = integral_workspace
+        self.projectile = integral_workspace.projectile
+        self.target = integral_workspace.target
+        self.sys = integral_workspace.sys
+        self.solver = integral_workspace.solver
+
+        # kinematic info
+        self.mu = integral_workspace.mu
+        self.Ecm = integral_workspace.Ecm
+        self.k = integral_workspace.k
+        self.eta = integral_workspace.eta
+
+        # precompute angular distributions in each partial wave
+        self.angles = angles
+        self.ls = self.integral_workspace.ls
+        self.P_l_costheta = eval_legendre(self.ls, np.cos(self.angles))
+        self.P_1_l_costheta = lpmv(1, self.ls, np.cos(self.angles))
+
+        # precompute things related to Coulomb interaction
+        self.sigma_l = self.integral_workspace.sigma_l
+        self.Zz = self.integral_workspace.Zz
+        self.k_c = self.integral_workspace.k_c
+        self.eta = self.integral_workspace.eta
+        if self.Zz > 0:
+            sin2 = np.sin(self.angles / 2.0) ** 2
+            self.f_c = (
+                -self.eta
+                / (2 * self.k * sin2)
+                * np.exp(
+                    2j * self.sigma_l[0]
+                    - 2j * self.eta * np.log(np.sin(self.angles / 2))
+                )
+            )
+            self.rutherford = 10 * self.eta**2 / (4 * self.k**2 * sin2**2)
+        else:
+            self.f_c = np.zeros_like(angles)
+            self.rutherford = np.zeros_like(angles)
 
     def xs(
         self,
@@ -191,8 +283,8 @@ class Workspace:
             rutherford = self.rutherford
             f_c = self.f_c
         else:
-            P_l_costheta = eval_legendre(self.sys.l[:, np.newaxis], np.cos(angles))
-            P_1_l_costheta = lpmv(1, self.sys.l[:, np.newaxis], np.cos(angles))
+            P_l_costheta = eval_legendre(self.ls, np.cos(angles))
+            P_1_l_costheta = lpmv(1, self.ls, np.cos(angles))
             sin2 = np.sin(angles / 2) ** 2
             rutherford = 10 * self.eta**2 / (4 * self.k**2 * sin2**2)
             f_c = (
@@ -201,15 +293,16 @@ class Workspace:
                 * np.exp(-1j * self.eta * np.log(sin2) + 2j * self.sigma_l[0])
             )
 
-        splus, sminus = self.smatrix(
+        splus, sminus = self.integral_workspace.smatrix(
             interaction_scalar, interaction_spin_orbit, args_scalar, args_spin_orbit
         )
         return ElasticXS(
-            *elastic_xs(
+            *differential_elastic_xs(
                 self.k,
                 angles,
                 splus,
                 sminus,
+                self.ls,
                 P_l_costheta,
                 P_1_l_costheta,
                 f_c,
@@ -220,11 +313,35 @@ class Workspace:
 
 
 @njit
-def elastic_xs(
+def integral_elastic_xs(
+    k: float,
+    Splus: np.array,
+    Sminus: np.array,
+    ls: np.array,
+    sigma_l: np.array = 0,
+):
+    xsrxn = 0.0
+    xst = 0.0
+
+    for l in range(Splus.shape[0]):
+        xsrxn += (l + 1) * (1 - np.absolute(Splus[l]) ** 2) + l * (
+            1 - np.absolute(Sminus[l]) ** 2
+        )
+        xst += (l + 1) * (1 - np.real(Splus[l])) + l * (1 - np.real(Sminus[l]))
+
+    xsrxn *= 10 * np.pi / k**2
+    xst *= 10 * 2 * np.pi / k**2
+
+    return xst, xsrxn
+
+
+@njit
+def differential_elastic_xs(
     k: float,
     angles: np.array,
     Splus: np.array,
     Sminus: np.array,
+    ls: np.array,
     P_l_theta: np.array,
     P_1_l_theta: np.array,
     f_c: np.array = 0,
