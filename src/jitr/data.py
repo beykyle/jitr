42cb9087364b3b053069069ce360b63c92c1cd3b
beykyle <beykyle@umich.edu>
2025-02-13 16:27:25 -0500

    add mass tables, replace old single AME table with a dataframe containing multiple masses

--- src/jitr/data.py
+++ src/jitr/data.py
@@ -1,3 +1,3 @@
 from pathlib import Path
-data_dir = Path(__file__).parent.resolve() / "../data/"
 
+data_dir = Path(__file__).parent.resolve() / "../data/"
--- src/jitr/reactions/kduq.py
+++ src/jitr/reactions/kduq.py
@@ -304,7 +304,11 @@ class KDGlobal:
         R_C = rv * A ** (1.0 / 3.0)
         if self.projectile == (1, 1):
             # Coulomb radius
-            rc0 = self.rc_0 + self.rc_A * A ** (-2.0 / 3.0) + self.rc_A2 * A ** (-5.0 / 3.0)
+            rc0 = (
+                self.rc_0
+                + self.rc_A * A ** (-2.0 / 3.0)
+                + self.rc_A2 * A ** (-5.0 / 3.0)
+            )
             R_C = rc0 * A ** (1.0 / 3.0)
 
             Vcbar = 1.73 / rc0 * Z * A ** (-1.0 / 3.0)
