eb7cd95a96c58dd566b58ab470d6f25e053c8e2a
beykyle <beykyle@umich.edu>
2025-04-27 14:25:03 -0400

    update nb

--- src/jitr/xs/quasielastic_pn.py
+++ src/jitr/xs/quasielastic_pn.py
@@ -1,88 +1,55 @@
-import pickle
 import numpy as np
 
 from scipy.special import sph_harm, gamma
 from sympy.physics.wigner import clebsch_gordan
 
+from .elastic import check_angles
 from ..utils import constants
-from ..utils.kinematics import (
-    ChannelKinematics,
-    classical_kinematics,
-    classical_kinematics_cm,
-)
-from ..utils.mass import mass, binding_energy
+from ..utils.kinematics import ChannelKinematics
 from ..reactions import (
+    Reaction,
     spin_half_orbit_coupling,
     ProjectileTargetSystem,
 )
 from ..rmatrix import Solver
 
 
-def kinematics(
-    target: tuple, analog: tuple, Elab: np.float64, Ex_IAS: np.float64, mass_kwargs={}
-):
-    mass_target = mass(*target, **mass_kwargs)[0]  # TODO use uncertainties
-    mass_analog = mass(*analog, **mass_kwargs)[0]
-    mn = constants.MASS_N
-    mp = constants.MASS_P
-    BE_target = binding_energy(*target, **mass_kwargs)[0]
-    BE_analog = binding_energy(*analog, **mass_kwargs)[0]
-    Q = BE_analog - BE_target - Ex_IAS
-    CDE = 1.33 * (target[1] + analog[1]) * 0.5 / target[0] ** (1.0 / 3.0)
-    kinematics_entrance = classical_kinematics(mass_target, mp, Elab, Zz=target[1])
-    Ecm_exit = kinematics_entrance.Ecm + Q
-    Elab_n, kinematics_exit = classical_kinematics_cm(mass_analog, mn, Ecm_exit)
-    return kinematics_entrance, kinematics_exit, Elab_n, Q, CDE
-
-
 class System:
     r"""
-    Stores physics parameters of system. Calculates useful parameters for each partial wave.
     """
 
     def __init__(
         self,
-        channel_radius_fm: np.float64,
-        lmax: np.int64,
-        target: tuple,
-        analog: tuple,
-        mass_target: np.float64,
-        mass_analog: np.float64,
-        kp: np.float64,
-        kn: np.float64,
+        channel_radius_fm: float,
+        lmax: int,
+        reaction: Reaction,
+        kinematics_entrance: ChannelKinematics,
+        kinematics_exit: ChannelKinematics,
     ):
         r"""
-        @params
-            channel_radius_fm (np.float64): channel radius in fm
         """
 
         self.channel_radius_fm = channel_radius_fm
         self.lmax = lmax
-        self.target = target
-        self.analog = analog
-        self.mass_target = mass_target
-        self.mass_analog = mass_analog
-        self.mass_p = mass(1, 1)
-        self.mass_n = mass(1, 0)
         self.l = np.arange(0, lmax + 1, dtype=np.int64)
 
         self.entrance = ProjectileTargetSystem(
-            channel_radius=self.channel_radius_fm * kp,
+            channel_radius=self.channel_radius_fm * kinematics_entrance.k,
             lmax=self.lmax,
-            mass_target=self.mass_target,
-            mass_projectile=self.mass_p,
-            Ztarget=self.target[1],
-            Zproj=1,
+            mass_target=reaction.target.m0,
+            mass_projectile=reaction.projectile.m0,
+            Ztarget=reaction.target.Z,
+            Zproj=reaction.projectile.Z,
             coupling=spin_half_orbit_coupling,
         )
 
         self.exit = ProjectileTargetSystem(
-            channel_radius=self.channel_radius_fm * kn,
+            channel_radius=self.channel_radius_fm * kinematics_exit.k,
             lmax=self.lmax,
-            mass_target=self.mass_analog,
-            mass_projectile=self.mass_n,
-            Ztarget=self.analog[1],
-            Zproj=0,
+            mass_target=reaction.residual.m0,
+            mass_projectile=reaction.product.m0,
+            Ztarget=reaction.residual.Z,
+            Zproj=reaction.product.Z,
             coupling=spin_half_orbit_coupling,
         )
 
@@ -92,70 +59,72 @@ class Workspace:
     Workspace for (p,n) quasi-elastic scattering observables for local interactions
     """
 
-    @classmethod
-    def load(obj, filename):
-        with open(filename, "rb") as f:
-            ws = pickle.load(f)
-            ws.solver = Solver(ws.nbasis)
-        return ws
-
-    def save(self, filename):
-        self.solver = None
-        with open(filename, "wb") as f:
-            pickle.dump(self, f)
-
     def __init__(
         self,
-        sys: System,
+        reaction: Reaction,
         kinematics_entrance: ChannelKinematics,
         kinematics_exit: ChannelKinematics,
-        Elab_entrance: np.float64,
-        Elab_exit: np.float64,
         solver: Solver,
         angles: np.array,
-        tmatrix_abs_tol: np.float64 = 1e-6,
+        lmax: int,
+        channel_radius_fm: float,
+        tmatrix_abs_tol: float = 1e-6,
     ):
-        assert np.all(np.diff(angles) > 0)
-        assert angles[0] >= 0.0 and angles[-1] <= np.pi
-        self.sys = sys
-        A = self.sys.target[0]
-        Z = self.sys.target[1]
-        N = A - Z
-        self.angles = angles
-        self.isovector_factor = np.sqrt(np.fabs(N - Z)) / (N - Z - 1)
+        # params
+        self.lmax = lmax,
+        self.channel_radius_fm = channel_radius_fm
+        self.tmatrix_abs_tol = tmatrix_abs_tol
+
+        # system
+        self.reaction = reaction
+        self.sys = System(
+            channel_radius_fm,
+            lmax,
+            reaction,
+            kinematics_entrance,
+            kinematics_exit,
+        )
+
+        # kinematics
         self.kinematics_entrance = kinematics_entrance
         self.kinematics_exit = kinematics_exit
-        self.Elab_entrance = Elab_entrance
-        self.Elab_exit = Elab_exit
         self.solver = solver
-        self.nbasis = solver.kernel.quadrature.nbasis
-        self.tmatrix_abs_tol = tmatrix_abs_tol
+
+        # angles
+        check_angles(angles)
+        self.angles = angles
+
+        # precompute for DWBA matrix element
+        A = self.reaction.target.A
+        Z = self.reaction.target.Z
+        N = A - Z
+        self.isovector_factor = np.sqrt(np.fabs(N - Z)) / (N - Z - 1)
 
         # precompute things for entrance channel
         self.free_matrices_p = self.solver.free_matrix(
-            sys.entrance.channel_radius, sys.l, coupled=False
+            self.sys.entrance.channel_radius, self.sys.l, coupled=False
         )
         self.basis_boundary_p = self.solver.precompute_boundaries(
-            sys.entrance.channel_radius
+            self.sys.entrance.channel_radius
         )
 
         # precompute things for exit channel
         self.free_matrices_n = self.solver.free_matrix(
-            sys.exit.channel_radius, sys.l, coupled=False
+            self.sys.exit.channel_radius, self.sys.l, coupled=False
         )
         self.basis_boundary_n = self.solver.precompute_boundaries(
-            sys.exit.channel_radius
+            self.sys.exit.channel_radius
         )
 
         # get partial wave information for entrance channel
-        channels, asymptotics = sys.entrance.get_partial_wave_channels(
+        channels, asymptotics = self.sys.entrance.get_partial_wave_channels(
             *self.kinematics_entrance
         )
         self.p_channels = [ch.decouple() for ch in channels]
         self.p_asymptotics = [asym.decouple() for asym in asymptotics]
 
         # get partial wave information for exit channel
-        channels, asymptotics = sys.exit.get_partial_wave_channels(
+        channels, asymptotics = self.sys.exit.get_partial_wave_channels(
             *self.kinematics_exit
         )
         self.n_channels = [ch.decouple() for ch in channels]
@@ -163,7 +132,7 @@ class Workspace:
 
         # l . s for p-wave and up
         self.l_dot_s = np.array(
-            [np.diag(coupling) for coupling in sys.entrance.couplings[1:]]
+            [np.diag(coupling) for coupling in self.sys.entrance.couplings[1:]]
         )
 
         # pre-compute purely geometric factors
